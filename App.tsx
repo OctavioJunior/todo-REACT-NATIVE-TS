405886cb659a9f918d93b26a064702031dbacff3
Octavio Chagas Junior <octaviopcjunior@hotmail.com>
2023-07-28 20:08:55 -0300

    feat: add async storage

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import {Home} from './src/screens/Home';
+import {TasksProvider} from './src/context/TaskContext';
 
 function App(): JSX.Element {
-  return <Home />;
+  return (
+    <TasksProvider>
+      <Home />
+    </TasksProvider>
+  );
 }
 
 export default App;
